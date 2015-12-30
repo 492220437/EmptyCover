566f694b8caf02d93b42a07f2f82c3777fae2038
HIGO1 <higo123@example.com>
2015-12-30 19:09:45 +0800

    Add Demo

---
+++ YFEmptyCoverDemo/YFEmptyCoverDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFEmptyCoverDemo
+//
+//  Created by YANGFENG on 15/12/30.
+//  Copyright © 2015年 杨峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
