2b90796a7ae0278028276d32de717eb71e75f3f5
HIGO1 <higo123@example.com>
2015-12-30 18:47:29 +0800

    Add File

---
+++ YFEmptyCover/EmptyCoverView.m
@@ -0,0 +1,21 @@
+//
+//  EmptyCoverView.m
+//  NoData
+//
+//  Created by YANGFENG on 15/9/18.
+//  Copyright (c) 2015年 杨峰. All rights reserved.
+//
+
+#import "EmptyCoverView.h"
+
+@implementation EmptyCoverView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
