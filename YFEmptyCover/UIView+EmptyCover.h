f142896f7f0c40631bb1f155d8e984722c812c26
HIGO1 <higo123@example.com>
2016-01-04 16:15:54 +0800

    修改逻辑，增加默认选项，使得不必实现协议也可以一行代码实现emptyCover，并固定emptyCover，是emptyCover不随着scrollview上下滑动

---
+++ YFEmptyCover/UIView+EmptyCover.h
@@ -0,0 +1,13 @@
+//
+//  UIView+EmptyCover.h
+//  YFEmptyCoverDemo
+//
+//  Created by YANGFENG on 16/1/4.
+//  Copyright © 2016年 杨峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (EmptyCover)
+
+@end
---
+++ YFEmptyCover/UIView+EmptyCover.m
@@ -0,0 +1,13 @@
+//
+//  UIView+EmptyCover.m
+//  YFEmptyCoverDemo
+//
+//  Created by YANGFENG on 16/1/4.
+//  Copyright © 2016年 杨峰. All rights reserved.
+//
+
+#import "UIView+EmptyCover.h"
+
+@implementation UIView (EmptyCover)
+
+@end
